1d66f8ffbcc9f200e419596a77fd8953312cf734
Vishnu Kantliwala - degreed <vkantliwala@degreed.com>
2025-03-31 20:08:19 +0530

    Fix: port

--- src/http/globalUrl.tsx
+++ src/http/globalUrl.tsx
@@ -1,15 +1,3 @@
-
 export const getBaseUrl = () => {
-    let url;
-    switch(process.env.NODE_ENV) {
-      case 'production':
-        url = 'http://93.188.167.217/api/';
-        break;
-      case 'development':
-      default:
-        url = 'http://localhost:8080/api/';
-    }
-  
-    return url;
-  }
-  
+  return process.env.REACT_BASE_URL || "http://localhost:8080/api/";
+};
